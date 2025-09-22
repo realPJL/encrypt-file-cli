83452c6dfb24f21a8bb7d4d6f27620916b1de596
PJL <pauljohannlorenz@proton.me>
2025-09-22 13:58:44 +0200

    Add error handling (back)

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"aes/internal/encryption"
 	"aes/internal/input"
+	"fmt"
+	"os"
 )
 
 func main() {
 	// No errors required as these are handled in the respective file
-	fileName, _ := input.ReadInput()
+	fileName, err := input.ReadInput()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	fileContent, _ := encryption.ReadContent(fileName)
+	fileContent, err := encryption.ReadContent(fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	ciphertext, _ := encryption.EncryptContent(fileContent)
+	ciphertext, err := encryption.EncryptContent(fileContent)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	encryption.CreateNewFile(fileName, ciphertext)
+	errCreate := encryption.CreateNewFile(fileName, ciphertext)
+	if errCreate != nil {
+		fmt.Printf("failed to create new file: %v", errCreate)
+		os.Exit(1)
+	}
 }
